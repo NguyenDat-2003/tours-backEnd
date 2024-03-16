ec94841587220f9d4448f5d07b71fa8ddeeb39d0
NguyenDat-2003 <maxdat2003@gmail.com>
2024-03-16 12:15:29 +0700

    Connect_mongodb

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,3 +1,30 @@
-/**
- * Updated by DatDev
- */
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+const MONGODB_URI = 'mongodb+srv://datdev:ok34BX1bvvhQrHmx@cluster-datdev.w1qqpiq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-DatDev'
+
+const DATABASE_NAME = 'tours-app-datdev'
+
+let toursDatabaseInstance = null
+
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect()
+
+  toursDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+}
+
+export const GET_DB = () => {
+  if (!toursDatabaseInstance) throw new Error('Must connect Database first')
+  return toursDatabaseInstance
+}
+
+export const CLOSE_DB = async () => {
+  await toursDatabaseInstance.close()
+}
--- src/server.js
+++ src/server.js
@@ -1,14 +1,35 @@
 import express from 'express'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8080
+  const hostname = 'localhost'
+  const port = 8080
 
-app.get('/', (req, res) => {
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.listen(port, hostname, () => {
-  console.log(`Hello Dat Dev, I am running at ${hostname}:${port}/`)
-})
+  app.listen(port, hostname, () => {
+    console.log(`Hello Dat Dev, I am running at ${hostname}:${port}/`)
+  })
+
+  exitHook(() => {
+    CLOSE_DB()
+  })
+}
+
+;(async () => {
+  try {
+    await CONNECT_DB()
+
+    // Khởi động server sau khi connect thành công
+    START_SERVER()
+  } catch (err) {
+    console.log(err)
+    process.exit(0)
+  }
+})()
