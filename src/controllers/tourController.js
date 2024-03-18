a95cf858e130dcc20e54c3ba771bae7b56027700
NguyenDat-2003 <maxdat2003@gmail.com>
2024-03-18 14:42:47 +0700

    api_create_tour_p2

--- src/controllers/tourController.js
+++ src/controllers/tourController.js
@@ -4,7 +4,6 @@ import { tourService } from '~/services/tourService'
 const createNew = async (req, res, next) => {
   try {
     const createdTour = await tourService.createNew(req.body)
-    console.log(createdTour)
     return res.status(StatusCodes.CREATED).json(createdTour)
   } catch (error) {
     next(error)
--- src/validations/tourValidation.js
+++ src/validations/tourValidation.js
@@ -5,11 +5,18 @@ import ApiError from '~/utils/ApiError'
 const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
     name: Joi.string().required().min(10).max(40).trim().strict(),
-    duration: Joi.number().required()
+    duration: Joi.number().required(),
+    maxGroupSize: Joi.number().required(),
+    difficulty: Joi.string().required().valid('easy', 'medium', 'difficult'),
+    price: Joi.number().required(),
+    priceDiscount: Joi.number().required(),
+
+    summary: Joi.string().required().trim().strict(),
+    imageCover: Joi.string().required().trim().strict()
   })
 
   try {
-    await correctCondition.validateAsync(req.body, { abortEarly: false })
+    await correctCondition.validateAsync(req.body, { abortEarly: false, allowUnknown: true })
     next()
   } catch (error) {
     next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, error.message))
