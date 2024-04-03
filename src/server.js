8bbe6f0ac901027b2bdd69b5e8e40dce1d93edee
NguyenDat-2003 <maxdat2003@gmail.com>
2024-04-03 16:27:01 +0700

    security_http_headers

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import exitHook from 'async-exit-hook'
+import helmet from 'helmet'
 
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
@@ -8,7 +9,9 @@ import { errorHandlingMiddleware } from './middlewares/errorHandlingMiddleware'
 
 const START_SERVER = () => {
   const app = express()
-  app.use(express.json())
+  app.use(helmet())
+
+  app.use(express.json({ limit: '10kb' }))
 
   app.use('/v1', APIs_V1)
 
